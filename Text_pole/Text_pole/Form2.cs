b5804047cff366a912ebadfb8322f2dc5aa344e0
Кирчанов Иван Павлович <rasulovyagub@gmail.com>
2024-06-14 11:22:53 +0500

    Project

---
+++ Text_pole/Text_pole/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Text_pole
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            textBox1.Text = "Ничто не истина, всё дозволено";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
